ea1f39e29feb53761f3c4a230f4c7f7c9c77839f
Vlad Drobyshev <vladdrobyshev1@gmail.com>
2023-03-02 08:14:55 +0200

    1

---
+++ develop/develop_2.java
@@ -0,0 +1,12 @@
+package develop;
+
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class develop_2 {
+    public static void code() {
+
+
+    }
+}
--- main.java
+++ main.java
@@ -1,8 +1,15 @@
 import javax.swing.JOptionPane;
 
+
 import lab_1.lab_1;
-import lab_2.lab_2;//підключення Лабараторних робіт
+import lab_2.lab_2;
+import lab_3.lab_3;
+
+import Lectures.menu;
+import Lectures.lec_5;
+
 import develop.develop_1;
+import develop.develop_2;
 import develop.learn;
 
 public class main {
@@ -12,14 +19,21 @@ public class main {
 
     public static void main(String args[]) {
         //develop_1.code();
-        //learn.code();
+        //develop_2.code();
+        lab_3.lab_3();
 
 
-        JOptionPane.showMessageDialog(null, "Дробишев Владислав\nВаріант-8");
+
+
+        /*JOptionPane.showMessageDialog(null, "Дробишев Владислав\nВаріант-8");
         int lab = Integer.parseInt(JOptionPane.showInputDialog("Оберіть лабараторну роботу\n"
                 + "- - - - - - - - - - - - - - - - - - - - - - - \n"
                 + "Lab_1 = 1\n"
                 + "Lab_2 = 2\n"
+                +"\n\n"
+
+                + "- - - - - - - - - - - - - - - - - - - - - - - \n"
+                + "Меню лекцій = 100\n"
                 + "Вихід = 0"));
 
 
@@ -27,6 +41,7 @@ public class main {
             case 0:
                 JOptionPane.showMessageDialog(null, "\uD83D\uDEAB Програму завершено \uD83D\uDEAB");
                 break;
+
             case 1:
                 JOptionPane.showMessageDialog(null, "↻ Завантаження " + lab + "-ї лаби ↻");
                 lab_1.lab_1();
@@ -35,12 +50,31 @@ public class main {
                 JOptionPane.showMessageDialog(null, "↻ Завантаження " + lab + "-ї лаби ↻");
                 lab_2.lab_2();
                 break;
+            case 3:
+                JOptionPane.showMessageDialog(null, "↻ Завантаження " + lab + "-ї лаби ↻");
+                lab_3.lab_3();
+                break;
+
+
+
+
+            case 100:
+                JOptionPane.showMessageDialog(null, "↻ Завантаження меню лекцій ↻");
+                menu.lectures();
+                break;
+
 
             default:
                 JOptionPane.showMessageDialog(null, "⚠️ Ви ввели недопустиме значення ⚠️");
                 break;
 
         }
+
+         */
+
+
+
+
 
 
 
